b2cbed6b50cfe1b618f61ff098dffc746a64ba4c
Camille CAMBIUM <ccambium@student.42.fr>
2023-03-09 02:35:49 -0500

    removing debug mode

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -4,6 +4,6 @@ sed -i s/"define( 'DB_NAME', 'database_name_here' );"/"define( 'DB_NAME', '$WORD
 sed -i s/"define( 'DB_USER', 'username_here' );"/"define( 'DB_USER', '$WORDPRESSUSER' );"/ /var/www/html/wordpress/wp-config.php
 sed -i s/"define( 'DB_PASSWORD', 'password_here' );"/"define( 'DB_PASSWORD', '$WORDPRESSPASSWORD' );"/ /var/www/html/wordpress/wp-config.php
 sed -i s/"define( 'DB_HOST', 'localhost' );"/"define( 'DB_HOST', 'mariadb' );"/ /var/www/html/wordpress/wp-config.php
-sed -i s/"define( 'WP_DEBUG', true );"/"define( 'WP_DEBUG', true );"/ /var/www/html/wordpress/wp-config.php
+# sed -i s/"define( 'WP_DEBUG', true );"/"define( 'WP_DEBUG', true );"/ /var/www/html/wordpress/wp-config.php # if debug needed
 echo "wp-config done !"
 exec "$@"
